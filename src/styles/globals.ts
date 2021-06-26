6d3c1ebaabb152267cc8669eaf2c2c8f4f9eab56
José Caldas <jrcaldas@yahoo.com.br>
2021-06-26 12:21:24 -0300

    Add ThemeProvider and styled-components.d.ts

--- src/styles/globals.ts
+++ src/styles/globals.ts
@@ -1,6 +1,7 @@
-import { createGlobalStyle } from "styled-components";
+import { createGlobalStyle, css } from "styled-components";
 
 export const GlobalStyles = createGlobalStyle`
+
 
 @font-face {
   font-family: 'Poppins';
@@ -38,15 +39,17 @@ export const GlobalStyles = createGlobalStyle`
      -webkit-font-smoothing: antialiased;
      -moz-osx-font-smoothing: grayscale;
  }
- html{
-   font-size: 62.5%;
- }
 
- html, body, #__next{
-     height: 100%;
- }
+${({ theme }) => css`
+  html {
+    font-size: 62.5%;
+  }
 
- body{
-     font-family:'Poppins',--apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen, Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif
- }
+  body {
+    font-family: ${theme.font.family};
+    font-size: ${theme.font.sizes.medium};
+  }
+`}
+
+ 
 `;
