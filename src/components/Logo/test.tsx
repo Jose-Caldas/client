09403caea4fa976eab9c7e0828470398a790a416
José Caldas <jrcaldas@yahoo.com.br>
2021-06-27 09:01:57 -0300

    Add color tests to Logo

--- src/components/Logo/test.tsx
+++ src/components/Logo/test.tsx
@@ -1,11 +1,23 @@
-import { render, screen } from "@testing-library/react";
+import { screen } from "@testing-library/react";
 
 //renderizar o component 'render'
 //selecionar o elemento 'screen' (queries) - getByLabel, getByText ...
-//expect - assertion - assercão, análise, comparação (neste caso renderizar a logo branca)
+//expect - assertion - assercão, análise, comparação (neste caso renderizar a logo branca e label Won Games - acessibilidade)
 
 import Logo from ".";
+import { renderWithTheme } from "../../utils/tests/helpers";
 
 describe("<Logo/>", () => {
-  it("should render a whote label by default", () => {});
+  it("should render a white label by default", () => {
+    renderWithTheme(<Logo />);
+    expect(screen.getByLabelText(/Won Games/i).parentElement).toHaveStyle({
+      color: "#FAFAFA",
+    });
+  });
+  it("should render a black label when color is passed", () => {
+    renderWithTheme(<Logo color="black" />);
+    expect(screen.getByLabelText(/Won Games/i).parentElement).toHaveStyle({
+      color: "#030517",
+    });
+  });
 });
