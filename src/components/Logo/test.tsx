942ae0b4edda1706186711beb9dc7edcad8820bd
José Caldas <jrcaldas@yahoo.com.br>
2021-06-27 09:28:13 -0300

    Add option to size on logo

--- src/components/Logo/test.tsx
+++ src/components/Logo/test.tsx
@@ -2,7 +2,7 @@ import { screen } from "@testing-library/react";
 
 //renderizar o component 'render'
 //selecionar o elemento 'screen' (queries) - getByLabel, getByText ...
-//expect - assertion - assercão, análise, comparação (neste caso renderizar a logo branca e label Won Games - acessibilidade)
+//expect - assertion - assercão, análise, comparação (neste caso renderizar a logo branca e label Won Games - acessibilidade, e teste de tamanho logo)
 
 import Logo from ".";
 import { renderWithTheme } from "../../utils/tests/helpers";
@@ -18,6 +18,18 @@ describe("<Logo/>", () => {
     renderWithTheme(<Logo color="black" />);
     expect(screen.getByLabelText(/Won Games/i).parentElement).toHaveStyle({
       color: "#030517",
+    });
+  });
+  it("should render a large logo", () => {
+    renderWithTheme(<Logo size="large" />);
+    expect(screen.getByLabelText(/Won Games/i).parentElement).toHaveStyle({
+      width: "20rem",
+    });
+  });
+  it("should render a normal logo when size is default", () => {
+    renderWithTheme(<Logo />);
+    expect(screen.getByLabelText(/Won Games/i).parentElement).toHaveStyle({
+      width: "11rem",
     });
   });
 });
