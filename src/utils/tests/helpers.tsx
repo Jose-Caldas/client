1abfec460ec1ecbf4e92d26a675918865e091921
José Caldas <jrcaldas@yahoo.com.br>
2021-06-28 11:46:51 -0300

    Add Button  tests

--- src/utils/tests/helpers.tsx
+++ src/utils/tests/helpers.tsx
@@ -2,7 +2,7 @@ import { ThemeProvider } from "styled-components";
 import { render, RenderResult } from "@testing-library/react";
 import theme from "../../styles/theme";
 
-//provider para renderizar testes em realeção a temas
+//provider para renderizar testes em realeção ao tema criado no projeto (theme.ts)
 
 export const renderWithTheme = (children: React.ReactNode): RenderResult =>
   render(<ThemeProvider theme={theme}>{children}</ThemeProvider>);
