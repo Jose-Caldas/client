362c00e5850e0bcd000ced6c0bef5fda351921d0
José Caldas <jrcaldas@yahoo.com.br>
2021-07-02 13:52:08 -0300

    Create match-media-mock and add Slider tests

--- src/components/Slider/test.tsx
+++ src/components/Slider/test.tsx
@@ -1,14 +1,26 @@
-// import { render, screen } from "@testing-library/react";
+import "../../../.jest/match-media-mock";
+import { render, screen } from "@testing-library/react";
 import "jest-styled-components";
 
-// import Slider from ".";
+import Slider from ".";
 // import { renderWithTheme } from "../../utils/tests/helpers";
 
 describe("<Slider/>", () => {
-  // it("should render the medium size by default", () => {
-  //   render(<Slider />);
-  //   expect(
-  //     screen.getByRole("heading", { name: /Slider/i })
-  //   ).toBeInTheDocument();
-  // });
+  it("should render children as slider item", () => {
+    const { container } = render(
+      <Slider settings={{ slidesToShow: 1, infinite: false }}>
+        <p>Item 1</p>
+        <p>Item 2</p>
+      </Slider>
+    );
+
+    expect(
+      screen.getByText(/item 1/i).parentElement?.parentElement
+    ).toHaveClass("slick-slide");
+
+    expect(
+      screen.getByText(/item 2/i).parentElement?.parentElement
+    ).toHaveClass("slick-slide");
+    expect(container.firstChild).toMatchSnapshot();
+  });
 });
