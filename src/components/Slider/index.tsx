ad1102224f8af554b88f5bac0c76c36146227e11
José Caldas <jrcaldas@yahoo.com.br>
2021-07-02 12:35:14 -0300

    add react-slick and Slider structure

---
+++ src/components/Slider/index.tsx
@@ -0,0 +1,19 @@
+import { ReactNode } from "react";
+import SlickSlider, { Settings } from "react-slick";
+
+import { Wrapper } from "./styles";
+
+export type SliderSettings = Settings;
+
+export type SliderProps = {
+  children: ReactNode;
+  settings: SliderSettings;
+};
+
+const Slider = ({ children, settings }: SliderProps) => (
+  <Wrapper>
+    <SlickSlider {...settings}>{children}</SlickSlider>
+  </Wrapper>
+);
+
+export default Slider;
---
+++ src/components/Slider/test.tsx
@@ -0,0 +1,14 @@
+// import { render, screen } from "@testing-library/react";
+import "jest-styled-components";
+
+// import Slider from ".";
+// import { renderWithTheme } from "../../utils/tests/helpers";
+
+describe("<Slider/>", () => {
+  // it("should render the medium size by default", () => {
+  //   render(<Slider />);
+  //   expect(
+  //     screen.getByRole("heading", { name: /Slider/i })
+  //   ).toBeInTheDocument();
+  // });
+});
