ab95e1e6cb60141657e1b070c213d6d19d432c30
José Caldas <jrcaldas@yahoo.com.br>
2021-07-08 13:30:38 -0300

    Add Auth initial styles

--- src/templates/Auth/index.tsx
+++ src/templates/Auth/index.tsx
@@ -12,21 +12,27 @@ export type AuthProps = {
 const Auth = ({ title, children }: AuthProps) => (
   <S.Wrapper>
     <S.BannerBlock>
-      <Logo />
-      <Heading>All your favorite games in one place</Heading>
-      <S.Subtitle>
-        <strong>WON</strong> is the best and most complete gaming platform.
-      </S.Subtitle>
-      <S.Footer>Won Games 2021 © Todos os Direitos Reservados</S.Footer>
+      <S.BannerContent>
+        <Logo />
+        <div>
+          <Heading>All your favorite games in one place</Heading>
+          <S.Subtitle>
+            <strong>WON</strong> is the best and most complete gaming platform.
+          </S.Subtitle>
+        </div>
+        <S.Footer>Won Games 2021 © Todos os Direitos Reservados</S.Footer>
+      </S.BannerContent>
     </S.BannerBlock>
 
-    <S.BannerContent>
-      <Logo color="black" size="large" />
-      <Heading lineLeft lineColor="secondary" color="black">
-        {title}
-      </Heading>
-      {children}
-    </S.BannerContent>
+    <S.Content>
+      <S.ContentWrapper>
+        <Logo color="black" size="large" />
+        <Heading lineLeft lineColor="secondary" color="black">
+          {title}
+        </Heading>
+        {children}
+      </S.ContentWrapper>
+    </S.Content>
   </S.Wrapper>
 );
 
