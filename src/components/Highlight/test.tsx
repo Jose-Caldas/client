d115dd11ac0239c76aa8f6e6cb08acb65b613e03
José Caldas <jrcaldas@yahoo.com.br>
2021-07-12 19:12:05 -0300

    Add Showcase tests

--- src/components/Highlight/test.tsx
+++ src/components/Highlight/test.tsx
@@ -6,60 +6,62 @@ import Highlight from ".";
 import { renderWithTheme } from "../../utils/tests/helpers";
 
 const props = {
-  title: "heading 1",
-  subtitle: "subtitle 1",
-  buttonLabel: "Buy now",
-  buttonLink: "/rdr2",
-  backgroundImage: "img/red-dead-img.jpg",
+    title: "heading 1",
+    subtitle: "subtitle 1",
+    buttonLabel: "Buy now",
+    buttonLink: "/rdr2",
+    backgroundImage: "img/red-dead-img.jpg",
 };
 
 describe("<Highlight/>", () => {
-  it("should render the headings and buttons", () => {
-    renderWithTheme(<Highlight {...props} />);
-    expect(
-      screen.getByRole("heading", { name: /heading 1/i })
-    ).toBeInTheDocument();
-    expect(
-      screen.getByRole("heading", { name: /subtitle 1/i })
-    ).toBeInTheDocument();
-    expect(screen.getByRole("link", { name: /buy now/i })).toBeInTheDocument();
-  });
-  it("should render background image", () => {
-    const { container } = renderWithTheme(<Highlight {...props} />);
-
-    expect(container.firstChild).toHaveStyle({
-      backgroundImage: `url(${props.backgroundImage})`,
+    it("should render the headings and buttons", () => {
+        renderWithTheme(<Highlight {...props} />);
+        expect(
+            screen.getByRole("heading", { name: /heading 1/i })
+        ).toBeInTheDocument();
+        expect(
+            screen.getByRole("heading", { name: /subtitle 1/i })
+        ).toBeInTheDocument();
+        expect(
+            screen.getByRole("link", { name: /buy now/i })
+        ).toBeInTheDocument();
     });
-  });
-  it("should render float image", () => {
-    renderWithTheme(<Highlight {...props} floatImage="/float-image.png" />);
+    it("should render background image", () => {
+        const { container } = renderWithTheme(<Highlight {...props} />);
 
-    expect(screen.getByRole("img", { name: props.title })).toHaveAttribute(
-      "src",
-      "/float-image.png"
-    );
-  });
-  it("should render align right by default", () => {
-    const { container } = renderWithTheme(<Highlight {...props} />);
-
-    expect(container.firstChild).toHaveStyleRule(
-      'grid-template-areas: "floatimage content"'
-    );
-    expect(container.firstChild).toHaveStyleRule("text-align", "right", {
-      modifier: `${S.Content}`,
+        expect(container.firstChild).toHaveStyle({
+            backgroundImage: `url(${props.backgroundImage})`,
+        });
     });
-  });
+    it("should render float image", () => {
+        renderWithTheme(<Highlight {...props} floatImage="/float-image.png" />);
 
-  it("should render align left by default", () => {
-    const { container } = renderWithTheme(
-      <Highlight {...props} alignment="left" />
-    );
-
-    expect(container.firstChild).toHaveStyleRule(
-      'grid-template-areas: " content floatimage"'
-    );
-    expect(container.firstChild).toHaveStyleRule("text-align", "left", {
-      modifier: `${S.Content}`,
+        expect(screen.getByRole("img", { name: props.title })).toHaveAttribute(
+            "src",
+            "/float-image.png"
+        );
     });
-  });
+    it("should render align right by default", () => {
+        const { container } = renderWithTheme(<Highlight {...props} />);
+
+        expect(container.firstChild).toHaveStyleRule(
+            'grid-template-areas: "floatimage content"'
+        );
+        expect(container.firstChild).toHaveStyleRule("text-align", "right", {
+            modifier: `${S.Content}`,
+        });
+    });
+
+    it("should render align left by default", () => {
+        const { container } = renderWithTheme(
+            <Highlight {...props} alignment="left" />
+        );
+
+        expect(container.firstChild).toHaveStyleRule(
+            'grid-template-areas: " content floatimage"'
+        );
+        expect(container.firstChild).toHaveStyleRule("text-align", "left", {
+            modifier: `${S.Content}`,
+        });
+    });
 });
--- src/components/Menu/test.tsx
+++ src/components/Menu/test.tsx
@@ -5,49 +5,55 @@ import Menu from ".";
 import { renderWithTheme } from "../../utils/tests/helpers";
 
 describe("<Menu/>", () => {
-  it("should render the menu", () => {
-    renderWithTheme(<Menu />);
+    it("should render the menu", () => {
+        renderWithTheme(<Menu />);
 
-    expect(screen.getByLabelText(/open menu/i)).toBeInTheDocument();
-    expect(screen.getByLabelText(/search/i)).toBeInTheDocument();
-    expect(screen.getByLabelText(/open shopping cart/i)).toBeInTheDocument();
-    expect(screen.getByRole("img", { name: /won games/i })).toBeInTheDocument();
-  });
-  it("should handle open/close mobile menu", () => {
-    renderWithTheme(<Menu />);
-    //selecionar o menu
-    const fullMenuElement = screen.getByRole("navigation", { hidden: true });
+        expect(screen.getByLabelText(/open menu/i)).toBeInTheDocument();
+        expect(screen.getByLabelText(/search/i)).toBeInTheDocument();
+        expect(
+            screen.getByLabelText(/open shopping cart/i)
+        ).toBeInTheDocument();
+        expect(
+            screen.getByRole("img", { name: /won games/i })
+        ).toBeInTheDocument();
+    });
+    it("should handle open/close mobile menu", () => {
+        renderWithTheme(<Menu />);
+        //selecionar o menu
+        const fullMenuElement = screen.getByRole("navigation", {
+            hidden: true,
+        });
 
-    // verificar se o menu está escondido (opacity:0)
-    expect(fullMenuElement.getAttribute("aria-hidden")).toBe("true");
-    expect(fullMenuElement).toHaveStyle({ opacity: 0 });
+        // verificar se o menu está escondido (opacity:0)
+        expect(fullMenuElement.getAttribute("aria-hidden")).toBe("true");
+        expect(fullMenuElement).toHaveStyle({ opacity: 0 });
 
-    // clicar no botão de abrir o menu e verificar se abriu
-    fireEvent.click(screen.getByLabelText(/open menu/i));
-    expect(fullMenuElement.getAttribute("aria-hidden")).toBe("false");
-    expect(fullMenuElement).toHaveStyle({ opacity: 1 });
+        // clicar no botão de abrir o menu e verificar se abriu
+        fireEvent.click(screen.getByLabelText(/open menu/i));
+        expect(fullMenuElement.getAttribute("aria-hidden")).toBe("false");
+        expect(fullMenuElement).toHaveStyle({ opacity: 1 });
 
-    // clicar no botão de fechar o menu e verificar se fechou
-    fireEvent.click(screen.getByLabelText(/close menu/i));
-    expect(fullMenuElement.getAttribute("aria-hidden")).toBe("true");
-    expect(fullMenuElement).toHaveStyle({ opacity: 0 });
-  });
-  it("should show register box when logged out", () => {
-    renderWithTheme(<Menu />);
+        // clicar no botão de fechar o menu e verificar se fechou
+        fireEvent.click(screen.getByLabelText(/close menu/i));
+        expect(fullMenuElement.getAttribute("aria-hidden")).toBe("true");
+        expect(fullMenuElement).toHaveStyle({ opacity: 0 });
+    });
+    it("should show register box when logged out", () => {
+        renderWithTheme(<Menu />);
 
-    expect(screen.getByText(/log in now/i)).toBeInTheDocument();
-    expect(screen.getByText(/sign up/i)).toBeInTheDocument();
+        // expect(screen.getByText(/sign in/i)).toBeInTheDocument();
+        expect(screen.getByText(/sign up/i)).toBeInTheDocument();
 
-    expect(screen.queryByText(/My account/i)).not.toBeInTheDocument();
-    expect(screen.queryByText(/Wishlist/i)).not.toBeInTheDocument();
-  });
-  it("should show whishlist in account when logged in", () => {
-    renderWithTheme(<Menu username="jrc" />);
+        expect(screen.queryByText(/My account/i)).not.toBeInTheDocument();
+        expect(screen.queryByText(/Wishlist/i)).not.toBeInTheDocument();
+    });
+    it("should show whishlist in account when logged in", () => {
+        renderWithTheme(<Menu username="jrc" />);
 
-    expect(screen.getByText(/My account/i)).toBeInTheDocument();
-    expect(screen.getByText(/Wishlist/i)).toBeInTheDocument();
+        expect(screen.getByText(/My account/i)).toBeInTheDocument();
+        expect(screen.getByText(/Wishlist/i)).toBeInTheDocument();
 
-    expect(screen.queryByText(/log in now/i)).not.toBeInTheDocument();
-    expect(screen.queryByText(/sign up/i)).not.toBeInTheDocument();
-  });
+        // expect(screen.queryByText(/sign in/i)).not.toBeInTheDocument();
+        expect(screen.queryByText(/sign up/i)).not.toBeInTheDocument();
+    });
 });
