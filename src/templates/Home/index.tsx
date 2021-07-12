856bd67810a248062468e2aacb86d7a6763de4b4
José Caldas <jrcaldas@yahoo.com.br>
2021-07-12 20:26:34 -0300

    Refactor Home to use Showcase

--- src/templates/Home/index.tsx
+++ src/templates/Home/index.tsx
@@ -2,106 +2,82 @@
 import { Container } from "../../components/Container";
 import Menu from "../../components/Menu";
 import Footer from "../../components/Footer";
-import Heading from "../../components/Heading";
 
 import { BannerProps } from "../../components/Banner";
 import { GameCardProps } from "../../components/GameCard";
-import Highlight, { HighlightProps } from "../../components/Highlight";
+import { HighlightProps } from "../../components/Highlight";
 
 import BannerSlider from "../../components/BannerSlider";
-import GameCardSlider from "../../components/GameCardSlider";
 
 import {
-  SectionBanner,
-  SectionNews,
-  SectionMostPopular,
-  SectionUpcoming,
-  SectionFreeGames,
-  SectionFooter,
+    SectionBanner,
+    SectionNews,
+    SectionUpcoming,
+    SectionFooter,
 } from "./styles";
+import Showcase from "../../components/Showcase";
 
 export type HomeTemplateProps = {
-  banners: BannerProps[];
-  newGames: GameCardProps[];
-  mostPopularHighlight: HighlightProps;
-  mostPopularGames: GameCardProps[];
-  upcommingGames: GameCardProps[];
-  upcommingHighlight: HighlightProps;
-  upcommingMoreGames: GameCardProps[];
-  freeGames: GameCardProps[];
-  freeHighlight: HighlightProps;
+    banners: BannerProps[];
+    newGames: GameCardProps[];
+    mostPopularHighlight: HighlightProps;
+    mostPopularGames: GameCardProps[];
+    upcommingGames: GameCardProps[];
+    upcommingHighlight: HighlightProps;
+    upcommingMoreGames: GameCardProps[];
+    freeGames: GameCardProps[];
+    freeHighlight: HighlightProps;
 };
 
 const Home = ({
-  banners,
-  newGames,
-  mostPopularHighlight,
-  mostPopularGames,
-  upcommingGames,
-  upcommingHighlight,
-  upcommingMoreGames,
-  freeGames,
-  freeHighlight,
+    banners,
+    newGames,
+    mostPopularHighlight,
+    mostPopularGames,
+    upcommingGames,
+    upcommingHighlight,
+    upcommingMoreGames,
+    freeGames,
+    freeHighlight,
 }: HomeTemplateProps) => (
-  <section>
-    <Container>
-      <Menu />
+    <section>
+        <Container>
+            <Menu />
+            <SectionBanner>
+                <BannerSlider items={banners} />
+            </SectionBanner>
+        </Container>
 
-      <SectionBanner>
-        <BannerSlider items={banners} />
-      </SectionBanner>
-    </Container>
+        <SectionNews>
+            <Showcase title="News" games={newGames} />
+        </SectionNews>
 
-    <SectionNews>
-      <Container>
-        <Heading lineLeft lineColor="secondary" color="black">
-          News
-        </Heading>
-        <GameCardSlider items={newGames} color="black" />
-      </Container>
-    </SectionNews>
-    <Container>
-      <SectionMostPopular>
-        <Heading lineLeft lineColor="secondary">
-          Most Popular
-        </Heading>
-        <Highlight
-          {...mostPopularHighlight}
-          floatImage="/img/red-dead-float.png"
+        <Showcase
+            title="Most Popular Games"
+            highlight={mostPopularHighlight}
+            games={mostPopularGames}
         />
-        <GameCardSlider items={mostPopularGames} />
-      </SectionMostPopular>
-    </Container>
-    <Container>
-      <SectionUpcoming>
-        <Heading lineLeft lineColor="secondary">
-          Up Comming
-        </Heading>
-        <GameCardSlider items={upcommingGames} />
-        <Highlight
-          {...upcommingHighlight}
-          floatImage="/img/red-dead-float.png"
-          alignment="left"
-        />
-        <GameCardSlider items={upcommingMoreGames} />
-      </SectionUpcoming>
-    </Container>
-    <Container>
-      <SectionFreeGames>
-        <Heading lineLeft lineColor="secondary">
-          Free Games
-        </Heading>
-        <Highlight {...freeHighlight} />
-        <GameCardSlider items={freeGames} />
-      </SectionFreeGames>
-    </Container>
 
-    <SectionFooter>
-      <Container>
-        <Footer />
-      </Container>
-    </SectionFooter>
-  </section>
+        <SectionUpcoming>
+            <Showcase title="Upcoming Games" games={upcommingGames} />
+            <Showcase
+                highlight={upcommingHighlight}
+                games={upcommingMoreGames}
+            />
+        </SectionUpcoming>
+
+        <Showcase
+            title=" Free Games"
+            highlight={freeHighlight}
+            games={freeGames}
+        />
+
+        <SectionFooter>
+            <Container>
+                <Footer />
+            </Container>
+        </SectionFooter>
+    </section>
 );
 
 export default Home;
