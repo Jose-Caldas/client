10c4be5690e542cf2d6fb7dd5b85baff8fb44bfb
José Caldas <jrcaldas@yahoo.com.br>
2021-07-14 14:51:18 -0300

    Add TextContent styles and tests

--- src/components/GameDetails/stories.tsx
+++ src/components/GameDetails/stories.tsx
@@ -1,22 +1,30 @@
 import { Story, Meta } from "@storybook/react/types-6-0";
-import GameDetails, { GameDetailsProps } from "../GameDetails";
+import GameDetails, { GameDetailsProps } from ".";
+import mockGame from "./mock";
 
 export default {
-    title: "GAME/GameDetails",
+    title: "Game/GameDetails",
     component: GameDetails,
     parameters: {
         backgrounds: {
             default: "won-dark",
         },
     },
-    args: {
-        platforms: ["windows", "linux", "mac"],
-    },
+    args: mockGame,
     argTypes: {
+        releaseDate: {
+            control: "date",
+        },
         platforms: {
             control: {
                 type: "inline-check",
                 options: ["windows", "linux", "mac"],
+            },
+        },
+        genres: {
+            control: {
+                type: "inline-check",
+                options: ["Role-playing", "Narrative", "Action", "Adventure"],
             },
         },
     },
--- src/components/Heading/styles.ts
+++ src/components/Heading/styles.ts
@@ -2,53 +2,53 @@ import styled, { css, DefaultTheme } from "styled-components";
 import media from "styled-media-query";
 import { HeadingProps, LineColors } from ".";
 
-const wrapperModifiers = {
-  small: (theme: DefaultTheme) => css`
-    font-size: ${theme.font.sizes.medium};
-    &::after {
-      width: 3rem;
-    }
-  `,
-  medium: (theme: DefaultTheme) => css`
-    font-size: ${theme.font.sizes.xlarge};
-    ${media.greaterThan("medium")`
+export const wrapperModifiers = {
+    small: (theme: DefaultTheme) => css`
+        font-size: ${theme.font.sizes.medium};
+        &::after {
+            width: 3rem;
+        }
+    `,
+    medium: (theme: DefaultTheme) => css`
+        font-size: ${theme.font.sizes.xlarge};
+        ${media.greaterThan("medium")`
     font-size: ${theme.font.sizes.xxlarge};
     `}
-  `,
-  huge: (theme: DefaultTheme) => css`
-    font-size: ${theme.font.sizes.huge};
-  `,
+    `,
+    huge: (theme: DefaultTheme) => css`
+        font-size: ${theme.font.sizes.huge};
+    `,
 
-  lineLeft: (theme: DefaultTheme, lineColor: LineColors) => css`
-    padding-left: ${theme.spacings.xxsmall};
-    border-left: 0.7rem solid ${theme.colors[lineColor]};
-  `,
+    lineLeft: (theme: DefaultTheme, lineColor: LineColors) => css`
+        padding-left: ${theme.spacings.xxsmall};
+        border-left: 0.7rem solid ${theme.colors[lineColor]};
+    `,
 
-  lineBottom: (theme: DefaultTheme, lineColor: LineColors) => css`
-    position: relative;
-    margin-bottom: ${theme.spacings.medium};
+    lineBottom: (theme: DefaultTheme, lineColor: LineColors) => css`
+        position: relative;
+        margin-bottom: ${theme.spacings.medium};
 
-    &::after {
-      position: absolute;
-      content: "";
-      width: 5rem;
-      border-bottom: 0.5rem solid ${theme.colors[lineColor]};
-      left: 0;
-      bottom: -1rem;
-    }
-  `,
+        &::after {
+            position: absolute;
+            content: "";
+            width: 5rem;
+            border-bottom: 0.5rem solid ${theme.colors[lineColor]};
+            left: 0;
+            bottom: -1rem;
+        }
+    `,
 };
 
 export const Wrapper = styled.h2<HeadingProps>`
-  ${({ theme, color, lineLeft, lineBottom, size, lineColor }) => css`
-    color: ${theme.colors[color!]};
-    /* font-size: ${theme.font.sizes.xlarge};
+    ${({ theme, color, lineLeft, lineBottom, size, lineColor }) => css`
+        color: ${theme.colors[color!]};
+        /* font-size: ${theme.font.sizes.xlarge};
     ${media.greaterThan("medium")`
     font-size: ${theme.font.sizes.xxlarge};
     `} */
 
-    ${lineLeft && wrapperModifiers.lineLeft(theme, lineColor!)}
-    ${lineBottom && wrapperModifiers.lineBottom(theme, lineColor!)}
+        ${lineLeft && wrapperModifiers.lineLeft(theme, lineColor!)}
+        ${lineBottom && wrapperModifiers.lineBottom(theme, lineColor!)}
     ${!!size && wrapperModifiers[size](theme)}
-  `}
+    `}
 `;
