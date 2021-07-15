61ceef07215fe5fdc1bbcbf7511b983416ae3142
José Caldas <jrcaldas@yahoo.com.br>
2021-07-15 18:20:24 -0300

    Add Wishlist initial tests

--- src/pages/wishlist.tsx
+++ src/pages/wishlist.tsx
@@ -1,7 +1,18 @@
-import Wishlist from "../templates/Wishlist";
+import Wishlist, { WishlistTemplateProps } from "../templates/Wishlist";
+import gamesMock from "../components/GameCardSlider/mock";
+import highlightMock from "../components/Highlight/mock";
 
-function WishlistPage() {
-    return <Wishlist />;
+function WishlistPage(props: WishlistTemplateProps) {
+    return <Wishlist {...props} />;
+}
+
+export async function getStaticProps() {
+    return {
+        props: {
+            recommendedGames: gamesMock.slice(0, 5),
+            recommendedHighlight: highlightMock,
+        },
+    };
 }
 
 export default WishlistPage;
