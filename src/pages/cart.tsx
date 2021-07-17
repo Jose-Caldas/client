fff167cf04ae22becd956dc590f80abd9d0ec5b9
José Caldas <jrcaldas@yahoo.com.br>
2021-07-16 21:12:07 -0300

    Add Cart Page and Template

---
+++ src/pages/cart.tsx
@@ -0,0 +1,23 @@
+import Cart, { CartTemplateProps } from "../templates/Cart";
+import gamesMock from "../components/GameCardSlider/mock";
+import highlightMock from "../components/Highlight/mock";
+import mockItems from "../components/CartList/mock";
+import cardsMock from "../components/PaymentOptions/mock";
+
+function CartPage(props: CartTemplateProps) {
+    return <Cart {...props} />;
+}
+
+export async function getServerSideProps() {
+    return {
+        props: {
+            recommendedGames: gamesMock.slice(0, 5),
+            recommendedHighlight: highlightMock,
+            items: mockItems,
+            total: "$ 330,00",
+            cards: cardsMock,
+        },
+    };
+}
+
+export default CartPage;
