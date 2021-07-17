e4fee2804638cbc31969c1239eba4d6b9d9ea312
José Caldas <jrcaldas@yahoo.com.br>
2021-07-17 09:07:59 -0300

    Add ProfileMenu styles and tests

--- src/components/ProfileMenu/test.tsx
+++ src/components/ProfileMenu/test.tsx
@@ -1,3 +1,5 @@
+import { screen } from "@testing-library/react";
+
 import "jest-styled-components";
 
 import ProfileMenu from ".";
@@ -5,8 +7,23 @@ import { renderWithTheme } from "../../utils/tests/helpers";
 
 describe("<ProfileMenu />", () => {
     it("should render the menu", () => {
-        renderWithTheme(<ProfileMenu />);
+        const { container } = renderWithTheme(<ProfileMenu />);
 
         // verificar os 4 links existentes
+        expect(
+            screen.getByRole("link", { name: /my profile/i })
+        ).toBeInTheDocument();
+
+        expect(
+            screen.getByRole("link", { name: /my cards/i })
+        ).toBeInTheDocument();
+        expect(
+            screen.getByRole("link", { name: /my orders/i })
+        ).toBeInTheDocument();
+        expect(
+            screen.getByRole("link", { name: /sign out/i })
+        ).toBeInTheDocument();
+
+        expect(container.firstChild).toMatchSnapshot();
     });
 });
