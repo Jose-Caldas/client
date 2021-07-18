ff7de18c93d16d693adb4e6a4b534f27b9c408e5
José Caldas <jrcaldas@yahoo.com.br>
2021-07-18 12:40:03 -0300

    Add Profile Template and /profile/me page

---
+++ src/templates/Profile/test.tsx
@@ -0,0 +1,14 @@
+import { render, screen } from "@testing-library/react";
+import "jest-styled-components";
+
+import Profile from ".";
+// import { renderWithTheme } from "../../utils/tests/helpers";
+
+describe("<Profile/>", () => {
+    it("should render the medium size by default", () => {
+        render(<Profile />);
+        expect(
+            screen.getByRole("heading", { name: /Profile/i })
+        ).toBeInTheDocument();
+    });
+});
