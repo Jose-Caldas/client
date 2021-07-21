c8d8684050044ddb2695009f1d15df301e9196ad
José Caldas <jrcaldas@yahoo.com.br>
2021-07-21 10:30:58 -0300

    Add ApolloProvider

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -3,20 +3,31 @@ import Head from "next/head";
 import { GlobalStyles } from "../styles/globals";
 import { ThemeProvider } from "styled-components";
 import theme from "../styles/theme";
+import { ApolloClient, ApolloProvider, InMemoryCache } from "@apollo/client";
 
 function App({ Component, pageProps }: AppProps) {
-  return (
-    <ThemeProvider theme={theme}>
-      <Head>
-        <title>Won Games</title>
-        <link rel="shortcut icon" href="/icon-512.png" />
-        <link rel="apple-touch-icon" href="/icon-512.png" />
-        <link rel="manifest" href="/manifest.json" />
-        <meta name="description" content="The definitive Game Store" />
-      </Head>
-      <Component {...pageProps} />
-      <GlobalStyles />
-    </ThemeProvider>
-  );
+    const client = new ApolloClient({
+        uri: "http://localhost:1337/graphql",
+        cache: new InMemoryCache(),
+    });
+
+    return (
+        <ApolloProvider client={client}>
+            <ThemeProvider theme={theme}>
+                <Head>
+                    <title>Won Games</title>
+                    <link rel="shortcut icon" href="/icon-512.png" />
+                    <link rel="apple-touch-icon" href="/icon-512.png" />
+                    <link rel="manifest" href="/manifest.json" />
+                    <meta
+                        name="description"
+                        content="The definitive Game Store"
+                    />
+                </Head>
+                <Component {...pageProps} />
+                <GlobalStyles />
+            </ThemeProvider>
+        </ApolloProvider>
+    );
 }
 export default App;
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,24 +1,38 @@
+import { gql, useQuery } from "@apollo/client";
+
 import Home, { HomeTemplateProps } from "../templates/Home";
 import bannersMock from "../components/BannerSlider/mock";
 import gamesMock from "../components/GameCardSlider/mock";
 import highlightMock from "../components/Highlight/mock";
 
 export default function Index(props: HomeTemplateProps) {
-  return <Home {...props} />;
+    const { data, loading, error } = useQuery(gql`
+        query getGames {
+            games {
+                name
+            }
+        }
+    `);
+
+    if (loading) return <p>Loading...</p>;
+    if (error) return <p>{error}</p>;
+    if (data) return <p>{JSON.stringify(data, null, 2)}</p>;
+
+    return <Home {...props} />;
 }
 
 export function getServerSideProps() {
-  return {
-    props: {
-      banners: bannersMock,
-      newGames: gamesMock,
-      mostPopularHighlight: highlightMock,
-      mostPopularGames: gamesMock,
-      upcommingGames: gamesMock,
-      upcommingHighlight: highlightMock,
-      upcommingMoreGames: gamesMock,
-      freeGames: gamesMock,
-      freeHighlight: highlightMock,
-    },
-  };
+    return {
+        props: {
+            banners: bannersMock,
+            newGames: gamesMock,
+            mostPopularHighlight: highlightMock,
+            mostPopularGames: gamesMock,
+            upcommingGames: gamesMock,
+            upcommingHighlight: highlightMock,
+            upcommingMoreGames: gamesMock,
+            freeGames: gamesMock,
+            freeHighlight: highlightMock,
+        },
+    };
 }
