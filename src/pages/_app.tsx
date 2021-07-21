9cec42935db432cf8b8e647f90a09169b878aaa0
José Caldas <jrcaldas@yahoo.com.br>
2021-07-21 11:40:18 -0300

    Config Apollo via SSR

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -3,14 +3,11 @@ import Head from "next/head";
 import { GlobalStyles } from "../styles/globals";
 import { ThemeProvider } from "styled-components";
 import theme from "../styles/theme";
-import { ApolloClient, ApolloProvider, InMemoryCache } from "@apollo/client";
+import { ApolloProvider } from "@apollo/client";
+import { useApollo } from "../utils/apollo";
 
 function App({ Component, pageProps }: AppProps) {
-    const client = new ApolloClient({
-        uri: "http://localhost:1337/graphql",
-        cache: new InMemoryCache(),
-    });
-
+    const client = useApollo(pageProps.InitialApolloState);
     return (
         <ApolloProvider client={client}>
             <ThemeProvider theme={theme}>
