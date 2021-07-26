07fa5fa148073f6e0353fd08c8d6d638e4103a80
José Caldas <jrcaldas@yahoo.com.br>
2021-07-26 19:46:24 -0300

    Fix Apollo initialState and Cache

--- src/templates/Games/test.tsx
+++ src/templates/Games/test.tsx
@@ -3,11 +3,14 @@ import { screen } from "@testing-library/react";
 import "jest-styled-components";
 import { renderWithTheme } from "../../utils/tests/helpers";
 import { MockedProvider } from "@apollo/client/testing";
-import { QUERY_GAMES } from "../../graphql/queries/games";
+import { fetchMoreMock, gamesMock } from "./mocks";
 
 import filterMock from "../../components/ExploreSidebar/mock";
 
 import GamesTemplate from ".";
+
+import userEvent from "@testing-library/user-event";
+import apolloCache from "../../utils/apolloCache";
 
 jest.mock("templates/Base", () => ({
     __esModule: true,
@@ -35,36 +38,7 @@ describe("<GamesTemplate />", () => {
     });
     it("should render sections", async () => {
         renderWithTheme(
-            <MockedProvider
-                mocks={[
-                    {
-                        request: {
-                            query: QUERY_GAMES,
-                            variables: { limit: 15 },
-                        },
-                        result: {
-                            data: {
-                                games: [
-                                    {
-                                        name: "RimWorld",
-                                        slug: "rimworld",
-                                        cover: {
-                                            url:
-                                                "/uploads/rimworld_8e93acc963.jpg",
-                                        },
-                                        developers: [
-                                            { name: "Ludeon Studios" },
-                                        ],
-                                        price: 65.99,
-                                        __typename: "Game",
-                                    },
-                                ],
-                            },
-                        },
-                    },
-                ]}
-                addTypename={false}
-            >
+            <MockedProvider mocks={[gamesMock]} addTypename={false}>
                 <GamesTemplate filterItems={filterMock} />
             </MockedProvider>
         );
@@ -78,10 +52,29 @@ describe("<GamesTemplate />", () => {
         expect(
             await screen.findByTestId("Mock ExploreSidebar")
         ).toBeInTheDocument();
-        expect(await screen.findByText(/RimWorld/i)).toBeInTheDocument();
+        expect(await screen.findByText(/Sample Game/i)).toBeInTheDocument();
 
         expect(
             await screen.findByRole("button", { name: /show more/i })
         ).toBeInTheDocument();
+    });
+
+    it("should render more games when show more is clicked", async () => {
+        renderWithTheme(
+            <MockedProvider
+                mocks={[gamesMock, fetchMoreMock]}
+                cache={apolloCache}
+            >
+                <GamesTemplate filterItems={filterMock} />
+            </MockedProvider>
+        );
+
+        expect(await screen.findByText(/Sample Game/i)).toBeInTheDocument();
+
+        userEvent.click(
+            await screen.findByRole("button", { name: /show more/i })
+        );
+
+        expect(await screen.findByText(/Fetch More Game/i)).toBeInTheDocument();
     });
 });
