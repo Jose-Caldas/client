db5eec92a731e9579a81a164e98dc56988417f5b
José Caldas <jrcaldas@yahoo.com.br>
2021-07-27 10:49:57 -0300

    Add tests to Games Template

--- src/templates/Games/test.tsx
+++ src/templates/Games/test.tsx
@@ -12,6 +12,17 @@ import GamesTemplate from ".";
 import userEvent from "@testing-library/user-event";
 import apolloCache from "../../utils/apolloCache";
 
+// eslint-disable-next-line @typescript-eslint/no-var-requires
+const useRouter = jest.spyOn(require("next/router"), "useRouter");
+const push = jest.fn();
+
+useRouter.mockImplementation(() => ({
+    push,
+    query: "",
+    asPath: "",
+    route: "/",
+}));
+
 jest.mock("templates/Base", () => ({
     __esModule: true,
     default: function Mock({ children }: { children: React.ReactNode }) {
@@ -19,46 +30,14 @@ jest.mock("templates/Base", () => ({
     },
 }));
 
-jest.mock("components/ExploreSidebar", () => ({
+jest.mock("next/link", () => ({
     __esModule: true,
     default: function Mock({ children }: { children: React.ReactNode }) {
-        return <div data-testid="Mock ExploreSidebar">{children}</div>;
+        return <div>{children}</div>;
     },
 }));
 
-describe("<GamesTemplate />", () => {
-    it("should render loading when starting the template", () => {
-        renderWithTheme(
-            <MockedProvider mocks={[]} addTypename={false}>
-                <GamesTemplate filterItems={filterMock} />
-            </MockedProvider>
-        );
-
-        expect(screen.getByText(/loading.../i)).toBeInTheDocument();
-    });
-    it("should render sections", async () => {
-        renderWithTheme(
-            <MockedProvider mocks={[gamesMock]} addTypename={false}>
-                <GamesTemplate filterItems={filterMock} />
-            </MockedProvider>
-        );
-        // it starts without data
-        // shows loading
-        expect(screen.getByText(/loading.../i)).toBeInTheDocument();
-        // we wait until we have data to get the elements
-        // get => tem certeza do elemento
-        // query => Não tem o elemento
-        // find => processos assincronos
-        expect(
-            await screen.findByTestId("Mock ExploreSidebar")
-        ).toBeInTheDocument();
-        expect(await screen.findByText(/Sample Game/i)).toBeInTheDocument();
-
-        expect(
-            await screen.findByRole("button", { name: /show more/i })
-        ).toBeInTheDocument();
-    });
-
+describe("<Games />", () => {
     it("should render more games when show more is clicked", async () => {
         renderWithTheme(
             <MockedProvider
@@ -76,5 +55,29 @@ describe("<GamesTemplate />", () => {
         );
 
         expect(await screen.findByText(/Fetch More Game/i)).toBeInTheDocument();
+    });
+
+    it("should change push router when selecting a filter", async () => {
+        renderWithTheme(
+            <MockedProvider
+                mocks={[gamesMock, fetchMoreMock]}
+                cache={apolloCache}
+            >
+                <GamesTemplate filterItems={filterMock} />
+            </MockedProvider>
+        );
+
+        userEvent.click(
+            await screen.findByRole("checkbox", { name: /windows/i })
+        );
+        userEvent.click(
+            await screen.findByRole("checkbox", { name: /linux/i })
+        );
+        userEvent.click(await screen.findByLabelText(/low to high/i));
+
+        expect(push).toHaveBeenCalledWith({
+            pathname: "/games",
+            query: { platforms: ["windows", "linux"], sort_by: "low-to-high" },
+        });
     });
 });
