cd49ac60c9b45407fcbf0831962ede10c17599fe
José Caldas <jrcaldas@yahoo.com.br>
2021-07-29 11:53:55 -0300

    Add methods to useCart

--- src/hooks/use-cart/test.tsx
+++ src/hooks/use-cart/test.tsx
@@ -4,10 +4,13 @@ import { setStorageItem } from "../../utils/localStorage";
 import { useCart, CartProvider, CartProviderProps } from ".";
 import { cartItems, gamesMock } from "./mock";
 import { MockedProvider } from "@apollo/client/testing";
-import { QueryGames_games } from "../../graphql/generated/QueryGames";
-import { cartMapper } from "../../utils/mappers";
+import { act } from "@testing-library/react";
 
 describe("useCart", () => {
+    beforeEach(() => {
+        window.localStorage.clear();
+    });
+
     it("should return items and its info if there are any in the cart", async () => {
         const wrapper = ({ children }: CartProviderProps) => (
             <MockedProvider mocks={[gamesMock]}>
@@ -21,36 +24,98 @@ describe("useCart", () => {
             wrapper,
         });
 
+        expect(result.current.loading).toBe(true);
+
         await waitForNextUpdate();
+
+        expect(result.current.loading).toBe(false);
 
         expect(result.current.items).toStrictEqual(cartItems);
         expect(result.current.quantity).toBe(2);
         expect(result.current.total).toBe("$21.00");
     });
 
-    describe("cartMapper()", () => {
-        it("should return empty array if no games", () => {
-            expect(cartMapper(undefined)).toStrictEqual([]);
+    it("should return true/false if the item is already in the cart", () => {
+        const wrapper = ({ children }: CartProviderProps) => (
+            <MockedProvider mocks={[gamesMock]}>
+                <CartProvider>{children}</CartProvider>
+            </MockedProvider>
+        );
+
+        setStorageItem("cartItems", ["1"]);
+
+        const { result } = renderHook(() => useCart(), {
+            wrapper,
         });
 
-        it("should return mapped items", () => {
-            const game = {
-                id: "1",
-                cover: {
-                    url: "/image.jpg",
-                },
-                name: "game",
-                price: 10,
-            } as QueryGames_games;
+        expect(result.current.isInCart("1")).toBe(true);
+        expect(result.current.isInCart("2")).toBe(false);
+    });
 
-            expect(cartMapper([game])).toStrictEqual([
-                {
-                    id: "1",
-                    img: "http://localhost:1337/image.jpg",
-                    title: "game",
-                    price: "$10.00",
-                },
-            ]);
+    it("should add item in the cart", () => {
+        const wrapper = ({ children }: CartProviderProps) => (
+            <MockedProvider mocks={[gamesMock]}>
+                <CartProvider>{children}</CartProvider>
+            </MockedProvider>
+        );
+
+        const { result } = renderHook(() => useCart(), {
+            wrapper,
         });
+
+        act(() => {
+            result.current.addToCart("1");
+        });
+
+        expect(result.current.quantity).toBe(1);
+        expect(window.localStorage.getItem("WONGAMES_cartItems")).toBe(
+            JSON.stringify(["1"])
+        );
+    });
+
+    it("should remove and item from the cart", () => {
+        const wrapper = ({ children }: CartProviderProps) => (
+            <MockedProvider mocks={[gamesMock]}>
+                <CartProvider>{children}</CartProvider>
+            </MockedProvider>
+        );
+
+        setStorageItem("cartItems", ["1"]);
+
+        const { result } = renderHook(() => useCart(), {
+            wrapper,
+        });
+
+        act(() => {
+            result.current.removeFromCart("1");
+        });
+
+        expect(result.current.quantity).toBe(0);
+        expect(window.localStorage.getItem("WONGAMES_cartItems")).toBe(
+            JSON.stringify([])
+        );
+    });
+
+    it("should clear the cart", () => {
+        const wrapper = ({ children }: CartProviderProps) => (
+            <MockedProvider mocks={[gamesMock]}>
+                <CartProvider>{children}</CartProvider>
+            </MockedProvider>
+        );
+
+        setStorageItem("cartItems", ["1"]);
+
+        const { result } = renderHook(() => useCart(), {
+            wrapper,
+        });
+
+        act(() => {
+            result.current.clearCart();
+        });
+
+        expect(result.current.quantity).toBe(0);
+        expect(window.localStorage.getItem("WONGAMES_cartItems")).toBe(
+            JSON.stringify([])
+        );
     });
 });
